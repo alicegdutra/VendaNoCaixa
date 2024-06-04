ae565d526f43f5a442cbd7c3a97f148adfc8a1f7
Alice Gimenez Dutra <alicegzdutra@gmail.com>
2024-06-04 15:52:57 -0300

    commit

--- VenderNoCaixa/Program.cs
+++ VenderNoCaixa/Program.cs
@@ -1,54 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using VenderNoCaixa;
 
-namespace InterfaceVendas
+List<Produto> produtos = new List<Produto>();
+List<Venda> vendas = new List<Venda>();
+
+
+
+int opcao = 0;
+while (opcao != 7)
 {
-    public class Venda
-    {
-        private int Data { get; set; }
-        private double Total { get; set; }
-
-        public List<ItemVenda> VItens {get; set;}
-
-        public Venda(int data, double total)
-        {
-            Data = data;
-            Total = total;
-        }
-        public void Mostrar()
-            {
-                Console.WriteLine("Data: " + Data + "\tTotal: " + Total);
-            }
-
-        public void AdicionarItem(ItemVenda item)
-        {
-
-            VItens.Add(item);
-        }
-        public void ItemVenda()
-        {
-            Console.WriteLine("\nListagem dos itens de venda: " + Total);
-            foreach (ItemVenda v in VItens)
-                v.Mostrar();
-        }
-
-        public double total
-        {
-            get{return Total;}
-            set{Total = value;}
-        }
-
-        public int data
-        {
-            get{return Data;}
-            set{Data = value;}
-        }
-        public int CalcularTotalItens()
-        {
-            Console.WriteLine(VItens.Count);
-            return VItens.Count;
-        }
+    System.Console.WriteLine(("Menu:"));
+    System.Console.WriteLine("1 - Cadastrar Produto:");
+    System.Console.WriteLine("2 - Registrar Venda");
+    System.Console.WriteLine("3 - Consultar Venda");
+    System.Console.WriteLine("4 - Forma de Pagamento (Cartão)");
+    System.Console.WriteLine("5 - Forma de Pagamento (Cheque)");
+    System.Console.WriteLine("6 - Forma de Pagamento (Espécie)");
+    System.Console.WriteLine("7 - Sair\nDigite a opção: ");
+    opcao = Convert.ToInt32(Console.ReadLine());
 }
+
+int op = 0;
+while (op != 4)
+{
+    Console.WriteLine("Menu Produtos e Vendas:");
+    Console.WriteLine("1 - Cadastrar Produto");
+    Console.WriteLine("2 - Registrar Venda");
+    Console.WriteLine("3 - Consultar Venda");
+    Console.WriteLine("4 - Sair\nDigite a opção: ");
+    op = Convert.ToInt32(Console.ReadLine());
+    if op = 1{
+        // Lógica para cadastrar um produto
+    }
+    if op = 2{
+        // Lógica para registrar uma venda
+    }
+    if op = 3{
+        // Lógica para consultar uma venda
+    }
+    if op = 4{
+        Console.WriteLine("Saindo...");
+    }
+}
+
+int opcao = 0;
+while (opcao != 4)
+{
+    Console.WriteLine("Menu Formas de Pagamento:");
+    Console.WriteLine("1 - Pagamento em Cartão");
+    Console.WriteLine("2 - Pagamento em Cheque");
+    Console.WriteLine("3 - Pagamento em Espécie");
+    Console.WriteLine("4 - Sair\nDigite a opção: ");
+    opcao = Convert.ToInt32(Console.ReadLine());
+    if op = 1{
+        // Lógica para pagamento no cartão
+    }
+    if op = 2{
+        // Lógica para pagamento em cheque
+    }
+    if op = 3{
+        // Lógica para pagamento em espécie
+    }
+    if op = 4{
+        Console.WriteLine("Saindo...");
+    }
+
 }
